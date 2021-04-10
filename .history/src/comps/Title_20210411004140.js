b25b53aac2ebe12494a12ff550584516526fdaa0
Anasooya2601 <anasooyadutta@gmail.com>
2021-04-11 01:08:38 +0530

    initial

---
+++ .history/src/comps/Title_20210411004140.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Title=()=>{
+    return(
+        <div className="title">
+            <h1>PicsGram</h1>
+<h2>YOUR Pictures</h2>
+        </div>
+    )
+}
---
+++ .history/src/comps/Title_20210411004226.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Title=()=>{
+    return(
+        <div className="title">
+            <h1>PicsGram</h1>
+<h2>YOUR Pictures</h2>
+<p>Lorem        </p>
+        </div>
+    )
+}
+export default title;
