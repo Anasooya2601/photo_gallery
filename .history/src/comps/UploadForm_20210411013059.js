e0b9f80d4a12851bcefba4aa2addaa3dea141998
Anasooya2601 <anasooyadutta@gmail.com>
2021-04-11 01:36:23 +0530

    initial

---
+++ .history/src/comps/UploadForm_20210411013059.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UploadForm=()=>{
+return(
+
+
+<form>
+    <input type="file"/>
+</form>
+
+)
+
+
+
+}
+export default UploadForm;
---
+++ .history/src/comps/UploadForm_20210411013219.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UploadForm=()=>{
+return(
+
+
+<form>
+    <input type="file" onChange={changeHandler}/>
+</form>
+
+)
+
+
+
+}
+export default UploadForm;
